1f637385fedb6b7f90d6b3e5d5bc153b2bf6dc25
RK <romainknezevic@gmail.com>
2017-04-17 10:51:52 +0200

    State(): keep track of current state of regenbox

--- regenbox/regenbox.go
+++ regenbox/regenbox.go
@@ -9,18 +9,31 @@ import (
 
 var ErrEmptyRead error = errors.New("message was empty")
 
+//go:generate stringer -type=ChargeState
 type ChargeState int
 
 const (
-	//go:generate stringer -type=ChargeState
 	Idle        ChargeState = ChargeState(ModeIdle)
 	Charging    ChargeState = ChargeState(ModeCharge)
 	Discharging ChargeState = ChargeState(ModeDischarge)
 )
 
+//go:generate stringer -type=State
+type State int
+
+const (
+	Disconnected    State = State(iota)
+	Connected       State = State(iota)
+	WriteError      State = State(iota)
+	ReadError       State = State(iota)
+	UnexpectedError State = State(iota)
+	NilBox          State = State(iota)
+)
+
 type RegenBox struct {
-	Conn  Connection
-	state ChargeState
+	Conn        Connection
+	chargeState ChargeState
+	state       State
 }
 
 func NewRegenBox(conn Connection) (rb *RegenBox, err error) {
@@ -31,11 +44,12 @@ func NewRegenBox(conn Connection) (rb *RegenBox, err error) {
 		}
 	}
 
-	rb = &RegenBox{conn, Idle}
+	rb = &RegenBox{conn, Idle, Connected}
 	buf, err := rb.read()
 	if err != nil {
 		return rb, err
 	} else if buf[0] != BoxReady {
+		rb.state = UnexpectedError
 		return rb, fmt.Errorf("unexpected ready byte %s vs. %d", buf[0], BoxReady)
 	}
 	return rb, nil
@@ -85,6 +99,13 @@ func (rb *RegenBox) SetIdle() error {
 }
 
 func (rb *RegenBox) ChargeState() ChargeState {
+	return rb.chargeState
+}
+
+func (rb *RegenBox) State() State {
+	if rb == nil {
+		return NilBox
+	}
 	return rb.state
 }
 
@@ -98,7 +119,7 @@ func (rb *RegenBox) SetChargeMode(mode byte) error {
 		return errors.New("no response from regenbox")
 	}
 	// no error, save state to box only now.
-	rb.state = ChargeState(mode)
+	rb.chargeState = ChargeState(mode)
 	return nil
 }
 
@@ -108,6 +129,7 @@ func (rb *RegenBox) talk(b byte) ([]byte, error) {
 	time.Sleep(time.Millisecond * 50) // small tempo
 	i, err := rb.Conn.Write([]byte{b})
 	if err != nil || i != 1 {
+		rb.state = WriteError
 		return nil, err
 	}
 	return rb.read()
@@ -118,11 +140,14 @@ func (rb *RegenBox) read() (buf []byte, err error) {
 	buf = make([]byte, 256)
 	i, err := rb.Conn.Read(buf)
 	if err != nil {
+		rb.state = ReadError
 		return buf[:i], err
 	}
 	out := trimCRLF(buf[:i])
 	if i == 0 || len(out) == 0 {
+		rb.state = UnexpectedError
 		return []byte{}, ErrEmptyRead
 	}
+	rb.state = Connected
 	return out, nil
 }
