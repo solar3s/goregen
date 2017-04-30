a99572b473b38b0f5c44dc5f0fc138c10482d8cc
RK <romainknezevic@gmail.com>
2017-04-30 12:11:54 +0200

    build.sh: removed darwin build, not supported yet

--- build.sh
+++ build.sh
@@ -2,14 +2,22 @@
 
 tmp=$(mktemp -d)
 
+echo "building linx-x64" >&2
 GOARCH=amd64 GOOS=linux go build -o $tmp/goregen-linux
+echo "building linx-x86" >&2
 GOARCH=386 GOOS=linux go build -o $tmp/goregen-linux-x86
+echo "building linx-arm" >&2
 GOARCH=arm GOOS=linux go build -o $tmp/goregen-linux-arm
 
-GOARCH=amd64 GOOS=darwin go build -o $tmp/goregen-darwin
-GOARCH=386 GOOS=darwin go build -o $tmp/goregen-darwin-x86
+echo "darwin not supported for now (blaming tarm/serial)" >&2
+#echo "building darwin-x64" >&2
+#GOARCH=amd64 GOOS=darwin go build -o $tmp/goregen-darwin
+#echo "building darwin-x86" >&2
+#GOARCH=386 GOOS=darwin go build -o $tmp/goregen-darwin-x86
 
+echo "building windows-x64" >&2
 GOARCH=amd64 GOOS=windows go build -o $tmp/goregen-win.exe
+echo "building windows-x86" >&2
 GOARCH=386 GOOS=windows go build -o $tmp/goregen-win-x86.exe
 
 
