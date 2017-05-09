93b93d61d568b9996f32b5859cb7a2ca6af00c2e
rkj <rk@jdid.co>
2017-05-09 18:54:09 +0200

    www: add /config, /snapshot endpoints

--- www/server.go
+++ www/server.go
@@ -1,6 +1,7 @@
 package www
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/solar3s/goregen/regenbox"
 	"html/template"
@@ -25,6 +26,49 @@ type RegenboxData struct {
 	State       string
 	ChargeState string
 	Voltage     string
+}
+
+// Config POST: s.Regenbox.SetConfig() (json encoded),
+//              Regenbox's must be stopped first
+//         GET: gets current s.Regenbox.Config()
+func (s *Server) Config(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		var cfg regenbox.Config
+		err := json.NewDecoder(r.Body).Decode(&cfg)
+		if err != nil {
+			log.Println("error decoding json:", err)
+			http.Error(w, "couldn't decode provided json", http.StatusUnprocessableEntity)
+			return
+		}
+
+		if !s.Regenbox.Stopped() {
+			http.Error(w, "regenbox must be stopped first", http.StatusNotAcceptable)
+			return
+		}
+		err = s.Regenbox.SetConfig(&cfg)
+		if err != nil {
+			log.Println("error setting config:", err)
+			http.Error(w, "error setting config (internal)", http.StatusInternalServerError)
+			return
+		}
+		break
+	case http.MethodGet:
+		break
+	default:
+		http.Error(w, fmt.Sprintf("unexpected http-method (%s)", r.Method), http.StatusMethodNotAllowed)
+		return
+	}
+
+	// encode regenbox config regardless of http method
+	w.WriteHeader(200)
+	_ = json.NewEncoder(w).Encode(s.Regenbox.Config())
+	return
+}
+
+// Snapshot encodes snapshot as json to w.
+func (s *Server) Snapshot(w http.ResponseWriter, r *http.Request) {
+	_ = json.NewEncoder(w).Encode(s.Regenbox.Snapshot())
 }
 
 func (s *Server) Home(w http.ResponseWriter, r *http.Request) {
@@ -77,6 +121,8 @@ func (s *Server) Start() {
 		watcher.WatchConn()
 	}()
 	go func() {
+		http.Handle("/config", Logger(http.HandlerFunc(s.Config), "config", s.Verbose))
+		http.Handle("/snapshot", Logger(http.HandlerFunc(s.Snapshot), "snapshot", s.Verbose))
 		http.Handle("/", Logger(http.HandlerFunc(s.Home), "www", s.Verbose))
 		log.Printf("listening on %s...", s.ListenAddr)
 		if err := http.ListenAndServe(s.ListenAddr, nil); err != nil {
